0259f9db4c35904b2908a503a211b0bbcf8df50f
Leilani-Lassalle <leilani.lassalle06@gmail.com>
2023-04-13 20:31:52 -0400

    modified js

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ function popUpFunction()
     console.log("PopUpFunction called successfully");
 }
 
-const form = document.querySelector("#contact-form");
-const submitButton = document.querySelector("#submit-button");
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
 const scriptURL = 'https://httpbin.org/post';
 
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  submitButton.disabled = true;
-  let requestBody = new FormData(form);
-  fetch(scriptURL, { method: 'POST', body: requestBody })
-    .then(response => {
-      if (response.ok) {
-        form.reset();
-        alert('Your message was sent successfully! We will get back to you soon.');
-      } else {
-        alert('There was an error sending your message. Please try again later.');
-      }
-      submitButton.disabled = false;
-    })
-    .catch(error => {
-      alert('There was an error sending your message. Please try again later.');
-      submitButton.disabled = false;
-    });
+    submitButton.disabled = true
+    e.preventDefault()
+    let requestBody = new FormData(form)
+    fetch(scriptURL, { method: 'POST', body: requestBody })
+        .then(response => {
+            alert('Success!', response)
+            submitButton.disabled = false
+        })
+        .catch(error => {
+            alert('Error!', error.message)
+            submitButton.disabled = false
+
+        }
+        )
 });
